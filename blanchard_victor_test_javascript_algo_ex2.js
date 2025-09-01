b93ba75fa49ac3de8cbc3827969138248684a73b
Victor Blanchard <viblanc@proton.me>
2025-09-01 16:24:37 +0200

    fix ex2

--- blanchard_victor_test_javascript_algo_ex2.js
+++ blanchard_victor_test_javascript_algo_ex2.js
@@ -1,5 +1,8 @@
 // check if an array contains valid and unique numbers
 function validArray(array) {
+    if (array.length !== SIZE)
+        return false;
+
     const numbersSet = new Set(array.filter(n => Number.isInteger(n) && n >= 1 && n <= 9));
     return numbersSet.size === array.length;
 }
